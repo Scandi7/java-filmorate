01d5f7e55f61ea4811ce4a58acc0f5e714b8f33b
Scandi <jukjukassa@gmail.com>
2024-09-27 00:21:47 +0700

    исправлены запросы бд

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,18 @@
-INSERT INTO genres (name)
-SELECT 'Комедия' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE name = 'Комедия');
-INSERT INTO genres (name)
-SELECT 'Драма' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE name = 'Драма');
-INSERT INTO genres (name)
-SELECT 'Мультфильм' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE name = 'Мультфильм');
-INSERT INTO genres (name)
-SELECT 'Триллер' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE name = 'Триллер');
-INSERT INTO genres (name)
-SELECT 'Документальный' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE name = 'Документальный');
-INSERT INTO genres (name)
-SELECT 'Боевик' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE name = 'Боевик');
+MERGE INTO genres (name)
+    KEY(name)
+    VALUES
+        ('Комедия'),
+    ('Драма'),
+    ('Мультфильм'),
+    ('Триллер'),
+    ('Документальный'),
+    ('Боевик');
 
-INSERT INTO mpa_ratings (rating, description)
-SELECT 'G', 'У фильма нет возрастных ограничений' WHERE NOT EXISTS (SELECT 1 FROM mpa_ratings WHERE rating = 'G');
-INSERT INTO mpa_ratings (rating, description)
-SELECT 'PG', 'Детям рекомендуется смотреть фильм с родителями' WHERE NOT EXISTS (SELECT 1 FROM mpa_ratings WHERE rating = 'PG');
-INSERT INTO mpa_ratings (rating, description)
-SELECT 'PG-13', 'Детям до 13 лет просмотр не желателен' WHERE NOT EXISTS (SELECT 1 FROM mpa_ratings WHERE rating = 'PG-13');
-INSERT INTO mpa_ratings (rating, description)
-SELECT 'R', 'Лицам до 17 лет просматривать фильм можно только в присутствии взрослого' WHERE NOT EXISTS (SELECT 1 FROM mpa_ratings WHERE rating = 'R');
-INSERT INTO mpa_ratings (rating, description)
-SELECT 'NC-17', 'Лицам до 18 лет просмотр запрещён' WHERE NOT EXISTS (SELECT 1 FROM mpa_ratings WHERE rating = 'NC-17');
+MERGE INTO mpa_ratings (rating, description)
+    KEY(rating)
+    VALUES
+    ('G', 'У фильма нет возрастных ограничений'),
+    ('PG', 'Детям рекомендуется смотреть фильм с родителями'),
+    ('PG-13', 'Детям до 13 лет просмотр не желателен'),
+    ('R', 'Лицам до 17 лет просматривать фильм можно только в присутствии взрослого'),
+    ('NC-17', 'Лицам до 18 лет просмотр запрещён');
