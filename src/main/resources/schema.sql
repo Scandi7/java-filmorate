75a18546617abd696235ebcf28232bb72ad4e599
Scandi <jukjukassa@gmail.com>
2024-09-26 23:32:07 +0700

    не запускается на гите

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS mpa_ratings
 (
     mpa_id      INT AUTO_INCREMENT PRIMARY KEY,
     rating      VARCHAR NOT NULL,
-    description VARCHAR,
-    UNIQUE (rating)
+    description VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS films
@@ -20,8 +19,7 @@ CREATE TABLE IF NOT EXISTS films
 CREATE TABLE IF NOT EXISTS genres
 (
     genre_id INT AUTO_INCREMENT PRIMARY KEY,
-    name     VARCHAR NOT NULL,
-    UNIQUE (name)
+    name     VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS film_genres
